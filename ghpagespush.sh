19b1cd50844452b8658a2460ec071eced9b3754a
george <george@tuan.pro>
2018-02-17 00:21:23 -0500

    Added in gh-pages push script

---
+++ ghpagespush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd public
+git status
+git add . -v
+git commit -m "$1"  > /dev/null
+if [ $? -eq 0 ]
+then
+  echo "Created git commit" $1
+  git push origin gh-pages
+  exit 0
+else
+  echo "No change to site."
+  exit 0
+fi
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
   "os"
 	"fmt"
+	"time"
 	"os/exec"
   "log"
   "net/http"
@@ -12,6 +13,7 @@ func main() {
 	port := os.Getenv("port")
 
 	http.HandleFunc("/hugobuild", HugoBuildCommand)
+	http.HandleFunc("/ghpagespush", GHPagesPushCommand)
 	
 	fs := http.FileServer(http.Dir("public/"))
 	http.Handle("/", fs)
@@ -28,6 +30,19 @@ func HugoBuildCommand(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	fmt.Fprintf(w, "%s\n", string(out))
+}
+
+func GHPagesPushCommand(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pushing GH-Pages branch\n")
+	
+	currentTime := time.Now().Format(time.UnixDate)
+	
+	fmt.Fprintf(w, currentTime)
+	
+	out, err := exec.Command("./ghpagespush.sh", currentTime).Output()
+	if err != nil { log.Fatal(err) }
 	
 	fmt.Fprintf(w, "%s\n", string(out))
 }
